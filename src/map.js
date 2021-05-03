b371ca752dda867f85d3819e435944f3c5851618
gillitorr <70658037+gillitorr@users.noreply.github.com>
2021-05-03 12:40:14 -0400

    Make prettier changes

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import { Loader } from "@googlemaps/js-api-loader";
+import { Loader } from '@googlemaps/js-api-loader';
 
 /*
  inspired by:
@@ -15,7 +15,6 @@ const apiQuery = async (id) => {
     .then((res) => res.json())
     .catch((err) => console.error(err));
 
-
   console.log(venueDetails.response.venue);
   return venueDetails;
 };
@@ -25,20 +24,20 @@ const mapInit = (apiKey, locations) => {
   // const center = { lat: 41.90476224706472, lng: 12.49822074385094 };
   const center = { lat: 38.747935611941074, lng: -98.54793617885777 };
   const zoom = 4;
-  const mapID = "a6ea77d474c830a3";
+  const mapID = 'a6ea77d474c830a3';
 
   const loader = new Loader({
     apiKey: apiKey,
-    version: "weekly",
+    version: 'weekly',
   });
 
-  const wrapper = document.getElementById("wrapper");
+  const wrapper = document.getElementById('wrapper');
 
   // Use static image instead of static map to reduce API call cost
 
   wrapper.style.backgroundImage = `url(https://images.unsplash.com/photo-1524661135-423995f22d0b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80)`;
 
-  wrapper.addEventListener("click", () => {
+  wrapper.addEventListener('click', () => {
     wrapper.remove();
     loader.load().then(() => {
       // mapOptions is an object that gets passed to the
@@ -56,7 +55,7 @@ const mapInit = (apiKey, locations) => {
         mapId: mapID,
       };
 
-      map = new google.maps.Map(document.getElementById("map"), mapOptions);
+      map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
       // Put additional map google code here
       // Put markers on the map and assign an infowindow to
@@ -69,7 +68,7 @@ const mapInit = (apiKey, locations) => {
           animation: google.maps.Animation.DROP,
           position: location.latlng,
         });
-        marker.addListener("click", toggleBounce);
+        marker.addListener('click', toggleBounce);
 
         function toggleBounce() {
           marker.setAnimation(google.maps.Animation.BOUNCE);
@@ -78,7 +77,7 @@ const mapInit = (apiKey, locations) => {
           }, 3000);
         }
 
-        marker.addListener("click", () => {
+        marker.addListener('click', () => {
           infowindow.setContent(location.title);
           infowindow.open(map, marker);
         });
